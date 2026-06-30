0798d2e9801dda96f709959e730469edd8ed42a2
ChandraMohan-teach <acm16082005@gmail.com>
2026-06-30 13:14:34 +0530

    Add Analytics component to main render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RangTarang from "./RangTarang.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RangTarang />
+    <>
+      <RangTarang />
+      <Analytics />
+    </>
   </React.StrictMode>
 );
