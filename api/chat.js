cccf74ee40f34c393163020e3c9b879d49fb9cbe
ChandraMohan-teach <acm16082005@gmail.com>
2026-08-25 17:57:12 +0530

    Increase max_tokens limit to 1000

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
             },
             ...messages,
           ],
-          max_tokens: 300,
+          max_tokens: 1000,
         }),
       }
     );
